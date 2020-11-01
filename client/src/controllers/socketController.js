587c8a8bf07ea7afa470032b10d0d0fd3f5f77ff
Otavio Bonder <otaviobonder@gmail.com>
2020-11-01 17:23:34 -0300

    improved socket communication

--- client/src/controllers/socketController.js
+++ client/src/controllers/socketController.js
@@ -9,7 +9,6 @@ const log = console.log;
 const io = new Socketio(3000, { serveClient: false });
 
 io.on('connection', (socket) => {
-    log(chalk.green(socket));
     ss(socket).on('fileDownload', (stream, data) => {
         stream.pipe(fs.createReadStream(`resources/${data.name}`));
     });
@@ -19,22 +18,26 @@ export const downloadFile = ({ file, host }) => {
     log(chalk.yellow('Criando uma conexão socket com o outro client...'));
     const socket = socketClient(`ws://${host}:3000`);
     socket.on('connect', () => {
-        log(chalk.green('Conexão estabelecida!'));
-        log(
-            chalk.yellow(
-                'Criando stream de comunicação e iniciando download do arquivo...'
-            )
-        );
-        const stream = ss.createStream();
-        ss(socket).emit('fileDownload', stream, { name: file });
-        stream.pipe(fs.createWriteStream(`resources/${file}`));
-        log(
-            chalk.green(
-                'Arquivo baixado com sucesso e salvo na pasta resources'
-            )
-        );
-        log(chalk.yellow('Encerrando conexão com o outro client...'));
-        socket.close();
-        log(chalk.green('Conexão encerrada!'));
+        if (socket.connected) {
+            log(chalk.green('Conexão estabelecida!'));
+            log(
+                chalk.yellow(
+                    'Criando stream de comunicação e iniciando download do arquivo...'
+                )
+            );
+            const stream = ss.createStream();
+            ss(socket).emit('fileDownload', stream, { name: file });
+            stream.pipe(fs.createWriteStream(`resources/${file}`));
+            log(
+                chalk.green(
+                    'Arquivo baixado com sucesso e salvo na pasta resources'
+                )
+            );
+            log(chalk.yellow('Encerrando conexão com o outro client...'));
+            socket.close();
+            log(chalk.green('Conexão encerrada!'));
+        } else {
+            log(chalk.red('Falha de conexão via socket'));
+        }
     });
 };
