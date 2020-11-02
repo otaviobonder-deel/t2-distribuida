a004b04aafeecf72d60daf2e701d803ffbf6fbd5
Otavio Bonder <otaviobonder@gmail.com>
2020-11-02 12:12:10 -0300

    fix socket

--- client/src/controllers/socketController.js
+++ client/src/controllers/socketController.js
@@ -1,20 +1,17 @@
 import socketClient from 'socket.io-client';
-import ss from 'socket.io-stream';
 import chalk from 'chalk';
 import fs from 'fs';
 import Socketio from 'socket.io';
 import { inquirerHelper } from './inquirerHelper.js';
 
-ss.forceBase64 = true;
-
 const log = console.log;
 
 const io = new Socketio(3000, { serveClient: false });
 
-io.on('connection', (socket) => {
-    ss(socket).on('fileDownload', (stream, data) => {
-        fs.createReadStream(`resources/${data.name}`).pipe(stream);
-    });
+io.on('fileDownload', ({ file }) => {
+    const fileReaded = fs.readFileSync(`resources/${file}`);
+    const base64File = new Buffer(fileReaded).toString('base64');
+    io.emit('file', base64File);
 });
 
 export const downloadFile = ({ file, host }) => {
@@ -28,21 +25,22 @@ export const downloadFile = ({ file, host }) => {
                     'Criando stream de comunicação e iniciando download do arquivo...'
                 )
             );
-            const stream = ss.createStream();
-            ss(socket).emit('fileDownload', stream, { name: file });
-            stream.pipe(fs.createWriteStream(`resources/${file}`));
-            log(
-                chalk.green(
-                    'Arquivo baixado com sucesso e salvo na pasta resources'
-                )
-            );
-            log(chalk.yellow('Encerrando conexão com o outro client...'));
-            socket.close();
-            log(chalk.green('Conexão encerrada!'));
-            inquirerHelper();
+            socket.emit('fileDownload', file);
         } else {
             log(chalk.red('Falha de conexão via socket'));
             inquirerHelper();
         }
+    });
+    socket.on('file', (data) => {
+        console.log(data);
+        log(
+            chalk.green(
+                'Arquivo baixado com sucesso e salvo na pasta resources'
+            )
+        );
+        log(chalk.yellow('Encerrando conexão com o outro client...'));
+        socket.close();
+        log(chalk.green('Conexão encerrada!'));
+        inquirerHelper();
     });
 };
